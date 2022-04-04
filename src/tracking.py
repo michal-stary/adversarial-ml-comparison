e66c50cd975d85692c2b4d94928132e7c236e679
Michal Stary <michalstary98@gmail.com>
2022-04-04 22:11:54 +0200

    clean tracker

--- src/tracking.py
+++ src/tracking.py
@@ -3,39 +3,22 @@ from adv_lib.distances.lp_norms import lp_distances
 from adv_lib.utils.losses import difference_of_logits, difference_of_logits_ratio
 from torch.nn.functional import cross_entropy
 from src.logging_ import Logger
-import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss
 
 
 class PyTorchModelTrackerBase:
-        def __init__(self, model, p, n_classes=10, logger=None):
+        def __init__(self, model, p, logger=None):
             self._model = model
             self._p = p
-
-
-            # TODO extract this from model
-            self.n_classes= n_classes
-
             self.logger = Logger() if logger is None else logger
-
-            # self._func_counter = torch.tensor(0)
-
-            # the first value is deemed to be the original input
-            # self._tracked_x = []
-            # self._tracked_pred = []
-            # self._tracked_acc = []
             self.reset()
 
         def __call__(self, x):
             # compute loss - register forward hook (compute loss, save)
             # compute distance (norm) - register forward hook (compute distance, save)
 
-            # TODO change to track only distances and not really the all images -- too large to fit into RAM
-            # self._tracked_x.append(x)  # customize as you want
             with torch.no_grad():
                 self._tracked_norm.append(lp_distances(self.inputs, x, p=self._p, dim=1))
                 self._func_counter += x.shape[0]
-
 
             pred = self._model.__call__(x)
 
@@ -47,26 +30,10 @@ class PyTorchModelTrackerBase:
         # call reset after each example
         def reset(self):
             self._func_counter = torch.tensor(0)
-            # self._tracked_x = list()
             self._tracked_norm= list()
             self._tracked_pred = list()
             self._tracked_acc = list()
 
-        @property
-        def tracked_x(self, value):
-            self._tracked_x = value
-
-        @tracked_x.getter
-        def tracked_x(self):
-            return torch.cat(self._tracked_x)
-
-        @property
-        def func_counter(self, value):
-            self._func_counter = value
-
-        @func_counter.getter
-        def func_counter(self):
-            return self._func_counter
 
         def __getattr__(self, attr):
             """
@@ -91,8 +58,8 @@ class PyTorchModelTrackerSetup(PyTorchModelTrackerBase):
     """
         Tracker class that requires setup call before each forward
     """
-    def __init__(self, model, p=float("inf"), loss_f="DL", n_classes=10, logger=None):
-        super().__init__(model, p, n_classes, logger)
+    def __init__(self, model, p=float("inf"), loss_f="DL", logger=None):
+        super().__init__(model, p, logger)
 
         if callable(loss_f):
             self._loss_f = loss_f
@@ -124,36 +91,3 @@ class PyTorchModelTrackerSetup(PyTorchModelTrackerBase):
 
             self.reset()
 
-class PyTorchModelTrackerRecompute(PyTorchModelTrackerBase):
-    """
-        Tracker class that AIM TO recompute the loss from delta grad at each call
-        - no external call is required
-        TODO - add backward hook on tensors passed to forward to extract the delta grads
-        TODO - recompute the loss from delta grads and inputs somehow
-    """
-    def __init__(self, model, p, loss_f, logger=None):
-        super().__init__(model, p, logger)
-
-    # call reset after each example
-    def reset(self):
-
-        # log crucial information
-        self.logger.log("norm_progress",
-            [lp_distances(self._tracked_x[0], x, p=self._p, dim=1) for x in self._tracked_x])
-
-        # self.logger.log("loss_progress",
-        #     [lp_distances(self._tracked_x[0], x, p=self._p, dim=1) for x in self._tracked_x])
-
-        self._func_counter = torch.tensor(0)
-        self._tracked_x = list()
-
-
-if __name__ == '__main__':
-    import torchvision
-
-    model = torchvision.models.resnet18(pretrained=True)
-    wrapped = PyTorchModelTracker(model)
-    inputs = torch.randn((4, 3, 224, 224))
-    outs = wrapped(inputs)
-    print(wrapped.tracked_x)
-    print(wrapped.func_counter)
